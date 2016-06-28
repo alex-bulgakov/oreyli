9484ddb9a639214355ee1cf7dcea2703af607ad3
alex-bulgakov <master8423@gmail.com>
2016-06-28 19:35:18 +0300

    work with script

--- test2/script.js
+++ test2/script.js
@@ -1,7 +1,11 @@
 var main = function() {
   "use strict";
   $("#sendComment").on("click", function (event){
-                       console.log("Hello, world!");
+    var $new_comment = $("<p>");
+    var comment_text = $("input.content__comment-input").val();
+    console.log(comment_text);
+    $new_comment.text(comment_text);
+    $(".content__comments").append($new_comment);
                        });
 };
 $(document).ready(main);
