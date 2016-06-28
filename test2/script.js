1f2fd562fd446e90ca54c1f1bae7d527c29629fd
alex-bulgakov <master8423@gmail.com>
2016-06-28 22:15:59 +0300

    done close comments functional

--- test2/script.js
+++ test2/script.js
@@ -1,26 +1,39 @@
 var main = function() {
   "use strict";
-  
+  var count = 1;
   var addCommentFromInputBox = function () {
     var $new_comment;
+    var $close_comment;
     if ($("input.content__comment-input").val() != "") {
-      $new_comment = $("<p>").text($("input.content__comment-input").val());
+      $new_comment = $("<p id='p" + count + "'>").text($("input.content__comment-input").val());
+      $close_comment = $("<div id='close_p" + count + "'>");
       $new_comment.hide();
       $(".content__comments").append($new_comment);
+      $(".content__comments p#p" + count).append($close_comment);
       $new_comment.fadeIn();
       $("input.content__comment-input").val("");
+      count += 1;
     }
     else {
       alert("Введите текст комментария");
     }
   }
   
+  var addEventClose = function () {
+     $("div[id^='close_p']").on("click", function(event){
+      var $p = event.target.id.split('_')[1]; 
+      $("#" + $p).remove();
+    });
+  }
+  
   $("#sendComment").on("click", function (event){
     addCommentFromInputBox();
+    addEventClose();
   });
   $("input.content__comment-input").on("keypress", function(event){
     if (event.keyCode == 13)  {
       addCommentFromInputBox();
+      addEventClose();
     }
   });
 };
