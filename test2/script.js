77f11063239eb2f5c750deab0a27cc349649dd4a
alex-bulgakov <master8423@gmail.com>
2016-06-28 19:13:18 +0300

    work with script

---
+++ test2/script.js
@@ -0,0 +1,7 @@
+var main = function() {
+  "use strict";
+  $("#sendComment").on("click", function (event){
+                       console.log("Hello, world!");
+                       });
+};
+$(document).ready(main);
