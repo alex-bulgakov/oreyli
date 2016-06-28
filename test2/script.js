be23ea3f588313611fe772a8dd8c9b762c0ad077
alex-bulgakov <master8423@gmail.com>
2016-06-28 21:06:24 +0300

    refactoring and improved script

--- test2/script.js
+++ test2/script.js
@@ -1,11 +1,27 @@
 var main = function() {
   "use strict";
+  
+  var addCommentFromInputBox = function () {
+    var $new_comment;
+    if ($("input.content__comment-input").val() != "") {
+      $new_comment = $("<p>").text($("input.content__comment-input").val());
+      $new_comment.hide();
+      $(".content__comments").append($new_comment);
+      $new_comment.fadeIn();
+      $("input.content__comment-input").val("");
+    }
+    else {
+      alert("Введите текст комментария");
+    }
+  }
+  
   $("#sendComment").on("click", function (event){
-    var $new_comment = $("<p>");
-    var comment_text = $("input.content__comment-input").val();
-    console.log(comment_text);
-    $new_comment.text(comment_text);
-    $(".content__comments").append($new_comment);
-                       });
+    addCommentFromInputBox();
+  });
+  $("input.content__comment-input").on("keypress", function(event){
+    if (event.keyCode == 13)  {
+      addCommentFromInputBox();
+    }
+  });
 };
 $(document).ready(main);
